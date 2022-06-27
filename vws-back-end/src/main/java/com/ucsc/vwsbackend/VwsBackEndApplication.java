13417f1737dd243c31686c1de6f17c6e8649a8e6
Chathura <chathura.madura11@gmail.com>
2022-06-27 22:44:10 +0530

    Test running

--- vws-back-end/src/main/java/com/ucsc/vwsbackend/VwsBackEndApplication.java
+++ vws-back-end/src/main/java/com/ucsc/vwsbackend/VwsBackEndApplication.java
@@ -8,6 +8,7 @@ public class VwsBackEndApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VwsBackEndApplication.class, args);
+		System.out.println("Hello,Running Successfully!");
 	}
 
 }
