391c4cd254e21ba97f0a8630628ac9ea8074efb5
Chathura <chathura.madura11@gmail.com>
2022-07-11 02:44:16 +0530

    add sidebar folder

--- vws-front-end/src/App.js
+++ vws-front-end/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import './Utilities/TestComponent'
+import Table from './Utilities/Table/Table';
+import Sidebar from './Utilities/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <h3>Hello</h3>
+      <div class="container">
+        <Table/>
+      </div>
     </div>
   );
 }
