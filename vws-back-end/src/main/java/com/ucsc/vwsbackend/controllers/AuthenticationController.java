980219c5c67b7ed73ee789ab3e3836f45c059880
Chathura <chathura.madura11@gmail.com>
2022-07-17 20:56:09 +0530

    create AuthenticationController with login, get user info

---
+++ vws-back-end/src/main/java/com/ucsc/vwsbackend/controllers/AuthenticationController.java
@@ -0,0 +1,73 @@
+package com.ucsc.vwsbackend.controllers;
+
+
+import java.security.NoSuchAlgorithmException;
+import java.security.Principal;
+import java.security.spec.InvalidKeySpecException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ucsc.vwsbackend.config.JWTTokenHelper;
+import com.ucsc.vwsbackend.entities.User;
+import com.ucsc.vwsbackend.requests.AuthenticationRequest;
+import com.ucsc.vwsbackend.responses.LoginResponse;
+import com.ucsc.vwsbackend.responses.UserInfo;
+
+@RestController
+@RequestMapping("/api/v1")
+@CrossOrigin
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    JWTTokenHelper jWTTokenHelper;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @PostMapping("/auth/login")
+    public ResponseEntity<?> login(@RequestBody AuthenticationRequest authenticationRequest) throws InvalidKeySpecException, NoSuchAlgorithmException {
+
+        final Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                authenticationRequest.getUserName(), authenticationRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        User user=(User)authentication.getPrincipal();
+        String jwtToken=jWTTokenHelper.generateToken(user.getUsername());
+
+        LoginResponse response=new LoginResponse();
+        response.setToken(jwtToken);
+
+        //200 OK response
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/auth/userinfo")
+    public ResponseEntity<?> getUserInfo(Principal user){
+        //call  service in here
+        User userObj=(User) userDetailsService.loadUserByUsername(user.getName());
+        //userDetailsService call to repo
+        UserInfo userInfo=new UserInfo();
+        userInfo.setFirstName(userObj.getFirstName());
+        userInfo.setLastName(userObj.getLastName());
+        userInfo.setRoles(userObj.getAuthorities().toArray());
+
+        return ResponseEntity.ok(userInfo);
+    }
+
+}
