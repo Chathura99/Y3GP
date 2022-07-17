b6c8e1e1fe4c43406109aab3bc3a4b255aa39c56
Chathura <chathura.madura11@gmail.com>
2022-07-17 21:42:28 +0530

    change controller

--- vws-back-end/src/main/java/com/ucsc/vwsbackend/controllers/HomeController.java
+++ vws-back-end/src/main/java/com/ucsc/vwsbackend/controllers/HomeController.java
@@ -4,11 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequestMapping("/api/v1")
 
-public class GuestController {
+public class HomeController  {
     @GetMapping("/vws-home")
     public String home(){
         return "Welcome to VWS!";
