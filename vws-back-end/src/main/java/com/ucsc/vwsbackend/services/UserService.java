c6daed7b746b6273b721ddb4b32f52133cca400b
Chathura <chathura.madura11@gmail.com>
2022-07-17 20:54:03 +0530

    create user service

---
+++ vws-back-end/src/main/java/com/ucsc/vwsbackend/services/UserService.java
@@ -0,0 +1,28 @@
+package com.ucsc.vwsbackend.services;
+
+import com.ucsc.vwsbackend.entities.User;
+import com.ucsc.vwsbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+    //link userRepository with UserDetailsService interface
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user=userRepository.findByUserName(username);
+
+        if(null==user) {
+            throw new UsernameNotFoundException("User Not Found with userName "+username);
+        }
+        return user;
+    }
+
+}
