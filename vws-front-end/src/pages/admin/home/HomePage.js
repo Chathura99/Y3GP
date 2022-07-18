0d0b4746dfa266a36f78aac2f1eee23d869caf67
Chathura <chathura.madura11@gmail.com>
2022-07-19 02:45:48 +0530

    create admin home component

---
+++ vws-front-end/src/pages/admin/home/HomePage.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Table from "../../../utilities/Table/Table";
+import DonutChart from "../../../utilities/Charts/DonutChart";
+
+export default function HomePage(){
+  return (
+    <div className="container-fluid">
+      <div className="row gutters">
+        <div className="col-xl-8 col-lg-8 col-md-12 col-sm-12 col-12">
+          <div className="card h-100">
+            <div className="card-body">
+              <div className="row gutters">Home</div>
+              <div className="row gutters ">line 1</div>
+              <div className="row gutters">line 2</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12 ">
+          <div className="card h-100">
+            <div className="card-body">
+            <div className="row gutters "><h3 className="ml-3">Title</h3></div>
+            <div className="row gutters "><DonutChart/></div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+
+      <div className="row gutters mt-3">
+        <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+          <div className="card h-100">
+            <div className="card-body"><Table/></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
