1e37cca830099296ec35f7ff3c6e843acb8ade12
Chathura <chathura.madura11@gmail.com>
2022-07-19 22:42:05 +0530

    small changes

--- vws-front-end/src/pages/admin/home/HomePage.js
+++ vws-front-end/src/pages/admin/home/HomePage.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from "react";
 import Table from "../../../utilities/Table/Table";
 import DonutChart from "../../../utilities/Charts/DonutChart";
 
-export default function HomePage(){
+export default function HomePage() {
   return (
     <div className="container-fluid">
       <div className="row gutters">
@@ -19,10 +19,13 @@ export default function HomePage(){
         <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12 ">
           <div className="card h-100">
             <div className="card-body">
-            <div className="row gutters "><h3 className="ml-3">Title</h3></div>
-            <div className="row gutters "><DonutChart/></div>
+              <div className="row gutters ">
+                <h3 className="ml-3">Title</h3>
+              </div>
+              <div className="row gutters ">
+                <DonutChart />
+              </div>
             </div>
-            
           </div>
         </div>
       </div>
@@ -30,10 +33,12 @@ export default function HomePage(){
       <div className="row gutters mt-3">
         <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
           <div className="card h-100">
-            <div className="card-body"><Table/></div>
+            <div className="card-body ">
+              <Table />
+            </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
