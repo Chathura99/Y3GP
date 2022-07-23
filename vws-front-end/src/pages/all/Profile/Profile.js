7bdb96c268c5ec86ce8f51d2a602b259f9de8b9b
Yashodha <yashodasubasinghe19@gmail.com>
2022-07-24 01:28:28 +0530

    Profile page

--- vws-front-end/src/pages/all/Profile/Profile.js
+++ vws-front-end/src/pages/all/Profile/Profile.js
@@ -85,128 +85,128 @@ export default function Profile() {
             <div className="card h-100" id="contentcard">
               <div className="card-body">
                 <form onSubmit={handleSubmit}>
-                <div className="row gutters ">
-                  <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                    <h6 className="mb-2 text-primary">Edit Profile</h6>
+                  <div className="row gutters ">
+                    <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                      <h6 className="mb-2 text-primary">Details</h6>
+                    </div>
+
+                    <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                      <div className="form-group ">
+                        <label for="fullName">Full Name</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="fullName"
+                          placeholder="Enter full name"
+                          value={profile.firstName}
+                          name="firstName"
+                          onChange={handleChange}
+                        />
+                      </div>
+                    </div>
+
+                    <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                      <div className="form-group">
+                        <label for="eMail">Email</label>
+                        <input
+                          type="email"
+                          className="form-control"
+                          id="eMail"
+                          placeholder="Enter email ID"
+                          value={profile.email}
+                          name="email"
+                          onChange={handleChange}
+                        />
+                      </div>
+                    </div>
+
+                    <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                      <div className="form-group">
+                        <label for="phone">Phone</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="phone"
+                          placeholder="Enter phone number"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                      <div className="form-group">
+                        <label for="website">University/School</label>
+                        <input
+                          type="url"
+                          className="form-control"
+                          id="website"
+                          placeholder="University or School"
+                        />
+                      </div>
+                    </div>
                   </div>
 
-                  <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                    <div className="form-group ">
-                      <label for="fullName">Full Name</label>
-                      <input
-                        type="text"
-                        className="form-control"
-                        id="fullName"
-                        placeholder="Enter full name"
-                        value={profile.firstName}
-                        name="firstName"
-                        onChange={handleChange}
-                      />
+                  <div className="row gutters">
+                    <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                      <h6 className="mt-3 mb-2 text-primary">Security</h6>
+                    </div>
+                    <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                      <div className="form-group">
+                        <label for="Street">Street</label>
+                        <input
+                          type="name"
+                          className="form-control"
+                          id="Street"
+                          placeholder="Enter Street"
+                        />
+                      </div>
+                    </div>
+                    <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                      <div className="form-group">
+                        <label for="ciTy">City</label>
+                        <input
+                          type="name"
+                          className="form-control"
+                          id="ciTy"
+                          placeholder="Enter City"
+                        />
+                      </div>
+                    </div>
+                    <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                      <div className="form-group">
+                        <label for="sTate">State</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="sTate"
+                          placeholder="Enter State"
+                        />
+                      </div>
                     </div>
                   </div>
 
-                  <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                    <div className="form-group">
-                      <label for="eMail">Email</label>
-                      <input
-                        type="email"
-                        className="form-control"
-                        id="eMail"
-                        placeholder="Enter email ID"
-                        value={profile.email}
-                        name="email"
-                        onChange={handleChange}
-                      />
+                  <div className="row gutters">
+                    <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                      <div className="text-center mt-3 ">
+                        <button
+                          type="button"
+                          id="submit"
+                          name="submit"
+                          className="btn btn-secondary m-2"
+                        >
+                          Cancel
+                        </button>
+                        <button
+                          type="button"
+                          id="submit"
+                          name="submit"
+                          className="btn btn-primary"
+                          onClick={handleSubmit}
+                        >
+                          Update
+                        </button>
+                      </div>
                     </div>
                   </div>
-
-                  <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                    <div className="form-group">
-                      <label for="phone">Phone</label>
-                      <input
-                        type="text"
-                        className="form-control"
-                        id="phone"
-                        placeholder="Enter phone number"
-                      />
-                    </div>
-                  </div>
-
-                  <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                    <div className="form-group">
-                      <label for="website">University/School</label>
-                      <input
-                        type="url"
-                        className="form-control"
-                        id="website"
-                        placeholder="University or School"
-                      />
-                    </div>
-                  </div>
-                </div>
-
-                <div className="row gutters">
-                  <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                    <h6 className="mt-3 mb-2 text-primary">Address</h6>
-                  </div>
-                  <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                    <div className="form-group">
-                      <label for="Street">Street</label>
-                      <input
-                        type="name"
-                        className="form-control"
-                        id="Street"
-                        placeholder="Enter Street"
-                      />
-                    </div>
-                  </div>
-                  <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                    <div className="form-group">
-                      <label for="ciTy">City</label>
-                      <input
-                        type="name"
-                        className="form-control"
-                        id="ciTy"
-                        placeholder="Enter City"
-                      />
-                    </div>
-                  </div>
-                  <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                    <div className="form-group">
-                      <label for="sTate">State</label>
-                      <input
-                        type="text"
-                        className="form-control"
-                        id="sTate"
-                        placeholder="Enter State"
-                      />
-                    </div>
-                  </div>
-                </div>
-                
-                <div className="row gutters">
-                  <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                    <div className="text-center mt-3 ">
-                      <button
-                        type="button"
-                        id="submit"
-                        name="submit"
-                        className="btn btn-secondary m-2"
-                      >
-                        Cancel
-                      </button>
-                      <button
-                        type="button"
-                        id="submit"
-                        name="submit"
-                        className="btn btn-primary"
-                        onClick={handleSubmit}
-                      >
-                        Update
-                      </button>
-                    </div>
-                  </div>
-                </div>
                 </form>
               </div>
             </div>
