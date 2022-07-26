0b128857656532d70c8df6e326423b253a687cc2
Chathura <chathura.madura11@gmail.com>
2022-07-27 02:50:41 +0530

    admin announcement view

--- vws-front-end/src/pages/admin/announcement/ViewAnnouncement.js
+++ vws-front-end/src/pages/admin/announcement/ViewAnnouncement.js
@@ -3,42 +3,30 @@ import React from "react";
 export default function ViewAnnouncement() {
   return (
     <>
-    <div className="container-fluid calculated-bodywidth" style={{}} id="bla">
-      <div className="row gutters mt-10">
-        <div className="col-xl-8 col-lg-8 col-md-12 col-sm-12 col-12">
-          <div className="card h-100" id="contentcard">
-            <div className="card-body">
-              <div className="row gutters">View Announcement</div>
-              <div className="row gutters ">line 1</div>
-              <div className="row gutters">line 2</div>
+      <div className="container-fluid calculated-bodywidth" style={{}} id="bla">
+        <div className="row gutters mt-10">
+          <div className="col-xl-8 col-lg-8 col-md-12 col-sm-12 col-12">
+            <div className="card h-100" id="contentcard">
+              <div className="card-body">
+                <h4 className="ml-3">Announcements</h4>
+                <div className="row gutters ">announcement 1</div>
+                <div className="row gutters">announcement 2</div>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12 ">
-          <div className="card h-100" id="contentcard">
-            <div className="card-body">
-              <div className="row gutters ">
-                <h3 className="ml-3">Title</h3>
-              </div>
-              <div className="row gutters ">
-                Content
+          <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12 ">
+            <div className="card h-100" id="contentcard">
+              <div className="card-body">
+                <div className="row gutters ">
+                  <h4 className="ml-3">Publish New Announcement</h4>
+                </div>
+                <div className="row gutters ">Add announcement form</div>
               </div>
             </div>
           </div>
         </div>
       </div>
-
-      <div className="row gutters mt-3">
-        <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-          <div className="card h-100" id="contentcard">
-            <div className="card-body ">
-             Content
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </>
+    </>
   );
 }
