8ef1649f6ef81f927de85eae1b69a4371ba9d6a2
Chathura <chathura.madura11@gmail.com>
2022-07-28 01:40:09 +0530

    create 2 form pop ups

--- vws-front-end/src/Utilities/PopUps/TopFormPopUp.js
+++ vws-front-end/src/Utilities/PopUps/TopFormPopUp.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-export default function FormPopUp() {
+export default function TopFormPopUp() {
   return (
     <div>
-      <button
+      {/* <button
         type="button"
         class="btn btn-primary"
         data-toggle="modal"
         data-target="#exampleModalCenter"
       >
         Middle
-      </button>
+      </button> */}
 
       <div
         class="modal fade"
@@ -35,18 +35,95 @@ export default function FormPopUp() {
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
-            <div class="modal-body">...</div>
-            <div class="modal-footer justify-content-center ">
-              <button
-                type="button"
-                class="btn btn-secondary pl-4 pr-4 pt-1 pb-1"
-                data-dismiss="modal"
-              >
-                Close
-              </button>
-              <button type="button" class="btn btn-primary pl-4 pr-4 pt-1 pb-1">
-                Save
-              </button>
+            <div class="modal-body">
+              <form onSubmit={""}>
+                <div className="row gutters ">
+                  <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                    <h4 className="mb-2">Details</h4>
+                  </div>
+
+                  <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div className="form-group ">
+                      <label for="fullName">Full Name</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="fullName"
+                        placeholder="Enter full name"
+                        // value={profile.firstName}
+                        name="firstName"
+                        // onChange={handleChange}
+                      />
+                    </div>
+                  </div>
+
+                  <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div className="form-group">
+                      <label for="eMail">Email</label>
+                      <input
+                        type="email"
+                        className="form-control"
+                        id="eMail"
+                        placeholder="Enter email ID"
+                        // value={profile.email}
+                        name="email"
+                        // onChange={handleChange}
+                      />
+                    </div>
+                  </div>
+
+                  <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div className="form-group">
+                      <label for="phone">Phone</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="phone"
+                        placeholder="Enter phone number"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div className="form-group">
+                      <label for="website">University/School</label>
+                      <input
+                        type="url"
+                        className="form-control"
+                        id="website"
+                        placeholder="University or School"
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="row gutters">
+                  <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                    <div className="text-center mt-3 ">
+                      <div class="modal-footer justify-content-center ">
+                        <button
+                          type="button"
+                          id="submit"
+                          name="submit"
+                          className="btn btn-secondary m-2"
+                          data-dismiss="modal"
+                        >
+                          Cancel
+                        </button>
+                        <button
+                          type="button"
+                          id="submit"
+                          name="submit"
+                          className="btn btn-primary"
+                          // onClick={handleSubmit}
+                        >
+                          Update
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </form>
             </div>
           </div>
         </div>
