5670bd550923a418d924218d889c4213f150e664
Yashodha <yashodasubasinghe19@gmail.com>
2022-08-07 21:08:10 +0530

    homepage changes

--- vws-front-end/src/pages/guestUser/home/HomePage.js
+++ vws-front-end/src/pages/guestUser/home/HomePage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "./homepage.css";
 import { Link } from 'react-router-dom';
 
+import Navbar from '../LandingPageNavbar/navbar';
 import UpcomingEvents from '../UpcomingEvents/UpcomingEvents';
 import OurProjects from "../OurProjects/OurProjects";
 import OurLatestEvents from "../OurLatestEvents/OurLatestEvents";
@@ -15,40 +16,9 @@ export default function HomePage() {
     <>
       <div className='home-cont'>
 
-        <nav class="navbar navbar-expand-lg navbar-light" id="Navbar">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Our Projects</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Support Us</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">About Us</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Contact Us</a>
-              </li>
-            </ul>
-
-            <Link to="/login" smooth={true} spy={true}>
-              <button class="btn my-2 my-sm-0" id="header-btn" Link to="/login" type="submit">Login</button>
-            </Link>
-            <Link to="/signup" smooth={true} spy={true}>
-              <button class="btn my-2 my-sm-0" id="header-btn-2" type="submit">Sign Up</button>
-            </Link>
-
-          </div>
-        </nav>
+        <div className='Navbar'>
+          <Navbar />
+        </div>
 
         <div className=" container-fluid" id="Home">
 
@@ -116,6 +86,10 @@ export default function HomePage() {
         <ContactUs />
       </div>
 
+
+
     </>
+
   )
+
 }
