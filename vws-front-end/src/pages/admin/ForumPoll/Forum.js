347d0c828f41a2451f52752bf14bf3b6ab8472ca
Shan-Dilranga <2019cs187@stu.ucsc.lk>
2022-08-09 13:58:50 +0530

    designed poll table

--- vws-front-end/src/pages/admin/ForumPoll/Forum.js
+++ vws-front-end/src/pages/admin/ForumPoll/Forum.js
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { useState, useEffect,useMemo } from "react";
+import EnhancedTable from "../../../utilities/Table/ForumTable";
+import "../ForumPoll/Forum.css"
+
 
 export default function Forum() {
+  
+
+  useEffect(() => {
+    checkValidate();
+  }, []);
+
+  const checkValidate = async () => {
+    const y = localStorage.getItem("USER_KEY");
+    if (!y) {
+      window.location.href = "/";
+    }
+  };
   return (
     <>
-      <h1> </h1>
-    </>
+<div className="container-fluid calculated-bodywidth" style={{}} id="bla">
+        <div className="row gutters mt-10">
+          
+
+          <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12 ">
+            <div className="card h-100" id="contentcard" style={{width:"37rem"}}>
+              <div className="card-body" >
+                
+                  <h3 className="ml-3"><center>Forum</center></h3>
+                  <button id='forumbtn'>Add a new forum topic</button>
+                
+              </div>
+            </div>
+          </div>
+          <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12 " style={{paddingLeft:"13rem"}}>
+            <div className="card h-100" id="contentcard" style={{width:"37rem"}}>
+              <div className="card-body">
+                
+                  <h3 className="ml-3"><center>Poll</center></h3>
+              
+                <EnhancedTable />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        
+      </div>
+          </>
   );
 }
