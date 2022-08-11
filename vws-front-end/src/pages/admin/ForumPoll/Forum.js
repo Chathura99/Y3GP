74aa24de8b3a45b5abf6a3f2619eaa7a781e9a9b
Shan-Dilranga <2019cs187@stu.ucsc.lk>
2022-08-12 00:16:07 +0530

    finished the forum table

--- vws-front-end/src/pages/admin/ForumPoll/Forum.js
+++ vws-front-end/src/pages/admin/ForumPoll/Forum.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect,useMemo } from "react";
 import EnhancedTable from "../../../utilities/Table/ForumTable";
-import "../ForumPoll/Forum.css"
+import "./Forum.css"
 import NewTable from "../../../utilities/Table/NewTable.js";
-import CoordinateEventForm from '../../volunteer/Projects/CoordinateEventForm.js';
-import ProposeProjectForm from '../../volunteer/Projects/ProposeProjectForm';
+
 
 
 export default function Forum() {
@@ -21,13 +20,13 @@ export default function Forum() {
   };
   const [ProjectsData, setProjectsData] = useState([
     {
-      project_name: "Ganitha Saviya",
-            description: "Ganitha Saviya is a ....",
-            idea_by: "Ravindu Perera",
-            date: "2022 09 12",
+      topic: "Blood Donation",
+      last_update: "2022 07 12",
+      replies: "05",
             
             
-            coordinate: (
+            
+      read: (
               <button
                 type="button"
                 id="submit"
@@ -39,63 +38,35 @@ export default function Forum() {
                 // #96BE25,#BE4D25
                 // onClick={handleSubmit}
               >
-                Coordinate
+                Read
               </button>
             ),
-            action: (
-              <button
-                type="button"
-                id="submit"
-                name="submit"
-                data-toggle="modal"
-                data-target="#exampleModalCenter"
-                className="btn p-1"
-                style={{backgroundColor:"#96BE25",border:"none",marginTop: 10,marginBottom: 10}}
-                // #96BE25,#BE4D25
-                // onClick={handleSubmit}
-              >
-                Project Details
-              </button>
-            ),
+            
           },
-          {
-            project_name: "Re-green Earth",
-            description: "Re-green Earth is a ...",
-            idea_by: "Sadaru Avishka",
-            date: "2022 09 02",
+    //       {
+    //         project_name: "Re-green Earth",
+    //         description: "Re-green Earth is a ...",
+    //         idea_by: "Sadaru Avishka",
+    //         date: "2022 09 02",
             
             
-            coordinate: (
-              <button
-                type="button"
-                id="submit"
-                name="submit"
-                data-toggle="modal"
-                data-target="#CoordinateEventForm"
-                className="btn p-1"
-                style={{backgroundColor:"#2596BE",border:"none",marginTop: 10,marginBottom: 10}}
-                // #96BE25-green,#BE4D25-red
-                // onClick={handleSubmit}
-              >
-                Coordinate
-              </button>
-            ),
-            action: (
-              <button
-                type="button"
-                id="submit"
-                name="submit"
-                data-toggle="modal"
-                data-target="#exampleModalCenter"
-                className="btn p-1"
-                style={{backgroundColor:"#96BE25",border:"none",marginTop: 10,marginBottom: 10}}
-                // #96BE25,#BE4D25
-                // onClick={handleSubmit}
-              >
-                Project Details
-              </button>
-            ),
-    }
+    //         read: (
+    //           <button
+    //             type="button"
+    //             id="submit"
+    //             name="submit"
+    //             data-toggle="modal"
+    //             data-target="#CoordinateEventForm"
+    //             className="btn p-1"
+    //             style={{backgroundColor:"#2596BE",border:"none",marginTop: 10,marginBottom: 10}}
+    //             // #96BE25-green,#BE4D25-red
+    //             // onClick={handleSubmit}
+    //           >
+    //             Read
+    //           </button>
+    //         ),
+            
+    // }
   ]);
 
 const data = useMemo(
@@ -104,12 +75,11 @@ const data = useMemo(
   const ProjectsHeadings=useMemo(
     () => [
      
-      { accessor: "project_name", Header: "PROJECT NAME" },
-      { accessor: "description", Header: "DESCRIPTION" },
-      { accessor: "idea_by", Header: "IDEA BY" },
-      { accessor: "date", Header: "DATE" },
-      { accessor: "coordinate", Header: "COORDINATE EVENT" },
-      { accessor: "action", Header: "PROJECT DETAILS" },
+      { accessor: "topic", Header: "TOPIC" },
+      { accessor: "last_update", Header: "LAST UPDATE" },
+      { accessor: "replies", Header: "REPLIES" },
+      { accessor: "read", Header: "ACTION" },
+      
       
     ],
     []
@@ -117,45 +87,42 @@ const data = useMemo(
 
   return (
     <>
-{/* <div className="container-fluid calculated-bodywidth" style={{}} id="bla">
-        <div className="row gutters mt-10">
-          
-
-          <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12 ">
-            <div className="card h-100" id="contentcard" style={{width:"37rem"}}>
-              <div className="card-body" >
+    <div  id="maindiv">
+    <div id="leftside">
+      <div className="container-fluid calculated-bodywidth"  id="blaa">
                 
-                  <h3 className="ml-3"><center>Forum</center></h3>
-                  <button id='forumbtn'>Add a new forum topic</button>
-                  <EnhancedTable />
-              </div>
-            </div>
-          </div>
-          <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12 " style={{paddingLeft:"13rem"}}>
-            <div className="card h-100" id="contentcard" style={{width:"37rem"}}>
-              <div className="card-body">
-                
-                  <h3 className="ml-3"><center>Poll</center></h3>
-              
-                <EnhancedTable />
-              </div>
-            </div>
-          </div>
-        </div>
 
-        
-      </div> */}
-      <div className="container-fluid calculated-bodywidth" style={{}} id="bla">
+                <div className="row gutters mt-3">
+                    <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                        <div className="card h-100" id="contentcard">
+                            <div className="card-body " >
+                                <h5>Forum</h5>
+                                
+                                  <button id='forumbtn' data-toggle="modal" data-target="#ProposeProjectForm">Add New Forum Topic </button>
+                                
+                                
+                                  <br></br><NewTable columns={ProjectsHeadings} data={ProjectsData}/>
+                                
+                                 
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div> 
+            <div id="rightside">
+            <div className="container-fluid calculated-bodywidth" style={{}} id="blaaa">
                 
 
                 <div className="row gutters mt-3">
                     <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                         <div className="card h-100" id="contentcard">
                             <div className="card-body ">
-                                <h5>Forum</h5>
+                                <h5>Poll</h5>
                                 
-                                  <button id='proposenewbtn' data-toggle="modal" data-target="#ProposeProjectForm">Add New Forum Topic </button>
-                                <h5> </h5>
+                                  <button id='forumbtn' data-toggle="modal" data-target="#ProposeProjectForm">Add New Poll </button>
+                                
                                 
                                   <br></br><NewTable columns={ProjectsHeadings} data={ProjectsData}/>
                                 {/* <ProposeProjectForm /> */}
@@ -166,6 +133,8 @@ const data = useMemo(
                         </div>
                     </div>
                 </div>
+            </div>
+            </div>
             </div>
           </>
   );
