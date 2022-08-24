ce64c9870d05959b67939d99c63cdd1c46825ff1
Yashodha <yashodasubasinghe19@gmail.com>
2022-08-24 20:18:34 +0530

    New changes

--- vws-front-end/src/pages/guestUser/UpcomingEvents/UpcomingEvents.js
+++ vws-front-end/src/pages/guestUser/UpcomingEvents/UpcomingEvents.js
@@ -13,14 +13,34 @@ export default function UpcomingEvents() {
 
                     <div className="card" id="eventcard">
                         <div className="card" id="eventcard-body">
-                            <img src="https://codingyaar.com/wp-content/uploads/bootstrap-4-card-image-left-demo-image.jpg?ezimgfmt=ng%3Awebp%2Fngcb1%2Frs%3Adevice%2Frscb1-1"
+                            <img src="http://csrbox.org/company/proj_img/1539235300World-Environment-Day.jpg"
                                 class="img-fluid" id="center"
                                 alt="..." />
 
                             <div className="card-body" id="event-body">
-                                <h5 className="card-title" id="title-1">Title</h5>
-                                <p className="card-text">Nam sollicitudinortor sit amet libero lobortis semper at et odio. In eu tellus tellus t
-                                    Pellentesque ullamcorper</p>
+                                <h5 className="card-title" id="title-1">Kumbuk Plantation Program</h5>
+                                <p className="card-text">
+                                    The 7th episode of Re Green Earth programme, will be held on 11th of September 2022,
+                                    in Kurunegala, along the Wariyapola Natiya - Malagane road by the Deduru Oya reservoir.
+                                </p>
+                            </div>
+
+                        </div>
+                    </div>
+
+
+                    <div className="card" id="eventcard">
+                        <div className="card" id="eventcard-body">
+                            <img src="http://csrbox.org/company/proj_img/1539235300World-Environment-Day.jpg"
+                                class="img-fluid" id="center"
+                                alt="..." />
+
+                            <div className="card-body" id="event-body">
+                                <h5 className="card-title" id="title-1">Kumbuk Plantation Program</h5>
+                                <p className="card-text">
+                                    The 7th episode of Re Green Earth programme, will be held on 11th of September 2022,
+                                    in Kurunegala, along the Wariyapola Natiya - Malagane road by the Deduru Oya reservoir.
+                                </p>
                             </div>
 
                         </div>
@@ -28,29 +48,16 @@ export default function UpcomingEvents() {
 
                     <div className="card" id="eventcard">
                         <div className="card" id="eventcard-body">
-                            <img src="https://codingyaar.com/wp-content/uploads/bootstrap-4-card-image-left-demo-image.jpg?ezimgfmt=ng%3Awebp%2Fngcb1%2Frs%3Adevice%2Frscb1-1"
+                            <img src="http://csrbox.org/company/proj_img/1539235300World-Environment-Day.jpg"
                                 class="img-fluid" id="center"
                                 alt="..." />
 
                             <div className="card-body" id="event-body">
-                                <h5 className="card-title" id="title-1">Title</h5>
-                                <p className="card-text">Nam sollicitudin diutate sed. Vestibulum sit amet tortor sit amet libero lobortis semper at et odio. In eu tellus tellus t
-                                    Pellentesque ullamcorper</p>
-                            </div>
-
-                        </div>
-                    </div>
-
-                    <div className="card" id="eventcard">
-                        <div className="card" id="eventcard-body">
-                            <img src="https://codingyaar.com/wp-content/uploads/bootstrap-4-card-image-left-demo-image.jpg?ezimgfmt=ng%3Awebp%2Fngcb1%2Frs%3Adevice%2Frscb1-1"
-                                class="img-fluid" id="center"
-                                alt="..." />
-
-                            <div className="card-body" id="event-body">
-                                <h5 className="card-title" id="title-1">Title</h5>
-                                <p className="card-text">Nam sovulputate sed. Vestibulum sit amet tortor sit amet libero lobortis semper at et odio. In eu tellus tellus t
-                                    Pellentesque ullamcorper</p>
+                                <h5 className="card-title" id="title-1">Kumbuk Plantation Program</h5>
+                                <p className="card-text">
+                                    The 7th episode of Re Green Earth programme, will be held on 11th of September 2022,
+                                    in Kurunegala, along the Wariyapola Natiya - Malagane road by the Deduru Oya reservoir.
+                                </p>
                             </div>
 
                         </div>
@@ -69,9 +76,9 @@ export default function UpcomingEvents() {
 
                     <div className="card" id="announcementcard">
                         <div className="card-body" id="announcement-body">
-                            <h5 className="card-title" id="title-2">Announcement Title</h5>
-                            <p className="card-text">Nam sollicitudin dignissim nunc, cursus ullamcorper eros vulputate sed. Vestibulum sit amet tortor sit amet libero lobortis semper at et odio. In eu tellus tellus t
-                                Pellentesque ullamcorper aliquet ultrices.</p>
+                            <h5 className="card-title" id="title-2">POSTPONED! The Ganitha Saviya Program at Vilgoda Maha Vidyalaya</h5>
+                            <p className="card-text">The Ganitha Saviya mathematical program at Vilgoda Maha Vidyalaya is postponed due to some unavoidable issues.
+                                Further notices will be published here.</p>
 
                             <div className="d-flex flex-row">
                                 <div className="p-2"><img src="https://static.vecteezy.com/system/resources/previews/002/318/271/original/user-profile-icon-free-vector.jpg"
@@ -80,7 +87,29 @@ export default function UpcomingEvents() {
                                     className="rounded-circle" />
                                 </div>
                                 <div className="comment-text w-100">
-                                    <h6 className="font-normal" id="name-right">James Anderson</h6>
+                                    <h6 className="font-normal" id="name-right">Yashodha Subhasinghe</h6>
+                                    <span className="m-b-15 d-block">Project Coordinator
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="card" id="announcementcard">
+                        <div className="card-body" id="announcement-body">
+                            <h5 className="card-title" id="title-2">POSTPONED! The Ganitha Saviya Program at Vilgoda Maha Vidyalaya</h5>
+                            <p className="card-text">The Ganitha Saviya mathematical program at Vilgoda Maha Vidyalaya is postponed due to some unavoidable issues.
+                                Further notices will be published here.</p>
+
+                            <div className="d-flex flex-row">
+                                <div className="p-2"><img src="https://static.vecteezy.com/system/resources/previews/002/318/271/original/user-profile-icon-free-vector.jpg"
+                                    alt="user"
+                                    width="40"
+                                    className="rounded-circle" />
+                                </div>
+                                <div className="comment-text w-100">
+                                    <h6 className="font-normal" id="name-right">Yashodha Subhasinghe</h6>
                                     <span className="m-b-15 d-block">Project Coordinator
                                     </span>
                                 </div>
@@ -90,9 +119,9 @@ export default function UpcomingEvents() {
 
                     <div className="card" id="announcementcard">
                         <div className="card-body" id="announcement-body">
-                            <h5 className="card-title" id="title-2">Announcement Title</h5>
-                            <p className="card-text">Nam sollicitudin dignissim nunc, cursus ullamcorper eros vulputate sed. Vestibulum sit amet tortor sit amet libero lobortis semper at et odio. In eu tellus tellus t
-                                Pellentesque ullamcorper aliquet ultrices.</p>
+                            <h5 className="card-title" id="title-2">POSTPONED! The Ganitha Saviya Program at Vilgoda Maha Vidyalaya</h5>
+                            <p className="card-text">The Ganitha Saviya mathematical program at Vilgoda Maha Vidyalaya is postponed due to some unavoidable issues.
+                                Further notices will be published here.</p>
 
                             <div className="d-flex flex-row">
                                 <div className="p-2"><img src="https://static.vecteezy.com/system/resources/previews/002/318/271/original/user-profile-icon-free-vector.jpg"
@@ -101,28 +130,7 @@ export default function UpcomingEvents() {
                                     className="rounded-circle" />
                                 </div>
                                 <div className="comment-text w-100">
-                                    <h6 className="font-normal" id="name-right">James Anderson</h6>
-                                    <span className="m-b-15 d-block">Project Coordinator
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="card" id="announcementcard">
-                        <div className="card-body" id="announcement-body">
-                            <h5 className="card-title" id="title-2">Announcement Title</h5>
-                            <p className="card-text">Nam sollicitudin dignissim nunc, cursus ullamcorper eros vulputate sed. Vestibulum sit amet tortor sit amet libero lobortis semper at et odio. In eu tellus tellus t
-                                Pellentesque ullamcorper aliquet ultrices.</p>
-
-                            <div className="d-flex flex-row">
-                                <div className="p-2"><img src="https://static.vecteezy.com/system/resources/previews/002/318/271/original/user-profile-icon-free-vector.jpg"
-                                    alt="user"
-                                    width="40"
-                                    className="rounded-circle" />
-                                </div>
-                                <div className="comment-text w-100">
-                                    <h6 className="font-normal" id="name-right">James Anderson</h6>
+                                    <h6 className="font-normal" id="name-right">Yashodha Subhasinghe</h6>
                                     <span className="m-b-15 d-block">Project Coordinator
                                     </span>
                                 </div>
