e63326d675e902b4c5e027469443ebdb41074684
Malik <2019cs193@stu.ucsc.lk>
2022-09-07 17:15:04 +0530

    querychange

--- vws-back-end/src/main/java/com/ucsc/vwsbackend/repository/EventDao/EventJdbcRepository.java
+++ vws-back-end/src/main/java/com/ucsc/vwsbackend/repository/EventDao/EventJdbcRepository.java
@@ -54,7 +54,7 @@ public class EventJdbcRepository {
                 "INNER JOIN project as p ON e.project_id=p.project_id " +
                 "INNER JOIN volunteer as v ON v.volunteer_id=p.volunteer_id " +
                 "INNER JOIN user as u ON u.id=v.id " +
-                "where e.status=1";
+                "and e.status=1";
 
         List<EventDetail> events = jdbc.query(query, new BeanPropertyRowMapper<EventDetail>(EventDetail.class));
         return events;
