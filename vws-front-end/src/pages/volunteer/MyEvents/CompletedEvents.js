61a5d0ef3b186d72e9dae5c6f064c0023bd6643e
Malik <2019cs193@stu.ucsc.lk>
2022-09-09 02:12:02 +0530

    completedEvent

--- vws-front-end/src/pages/volunteer/MyEvents/CompletedEvents.js
+++ vws-front-end/src/pages/volunteer/MyEvents/CompletedEvents.js
@@ -1,81 +1,36 @@
 import React, { useEffect,useState } from 'react';
-import Table from "../../../utilities/Table/Table";
 import DonutChart from "../../../utilities/Charts/DonutChart";
 import { LineChart } from './../../../utilities/Charts/LineChart';
-import { useMemo } from 'react';
-import NewTable from '../../../utilities/Table/NewTable';
+
+// for remove box shadow
+import { Paper } from "@material-ui/core";
+import MaterialTable from "material-table";
+import { getCompletedEvents } from '../../../services/eventServices/eventService';
 
 export default function CompletedEvents() {
-  const [upcomingEventsData, setUpComingEventsData] = useState([
-    {
-            event_id: "E001",
-            category: "Ganitha Saviya",
-            event_coordinator: "Ravindu",
-            startdate: "2022 09 12",
-            enddate: "2022 09 14",
-            no_of_members: "13",
-            location: "Nikawaratiya",
-            
-            action: (
-                      <button
-                        type="button"
-                        id="submit"
-                        name="submit"
-                        data-toggle="modal"
-                        data-target="#exampleModalCenter"
-                        className="btn p-1"
-                        style={{backgroundColor:"#96BE25",border:"none",marginTop: 10,marginBottom: 10}}
-                        // #96BE25,#BE4D25
-                        // onClick={handleSubmit}
-                      >
-                        Details
-                      </button>
-                    ),
-    },
-    {
-            event_id: "E002",
-            category: "Re-green Earth",
-            event_coordinator: "Sadaru",
-            startdate: "2022 09 02",
-            enddate: "2022 09 04",
-            no_of_members: "8",
-            location: "Horana",
-            
-            action: (
-              <button
-                type="button"
-                id="submit"
-                name="submit"
-                data-toggle="modal"
-                data-target="#exampleModalCenter"
-                className="btn p-1"
-                style={{backgroundColor:"#96BE25",border:"none",marginTop: 10,marginBottom: 10}}
-                // #96BE25,#BE4D25
-                // onClick={handleSubmit}
-              >
-                Details
-              </button>
-            ),
+
+  useEffect(() => {
+    checkValidate();
+    getCompletedEventDetails();
+
+  }, []);
+
+  const checkValidate = async () => {
+    const y = localStorage.getItem("USER_KEY");
+    if (!y) {
+      window.location.href = "/";
     }
-  ]);
+  };
 
-const data = useMemo(
-() => upcomingEventsData  )
+  const getCompletedEventDetails = async () => {
+    const res = await getCompletedEvents();
+    // console.log(res.data);
+    setCompletedEventData(res.data);
+  };
 
-  const UpcomingEventsHeadings=useMemo(
-    () => [
-     
-      { accessor: "event_id", Header: "EVENT ID" },
-      { accessor: "category", Header: "CATEGORY" },
-      { accessor: "event_coordinator", Header: "EVENT COORDINATOR" },
-      { accessor: "startdate", Header: "STARTS ON" },
-      { accessor: "enddate", Header: "ENDS ON" },
-      { accessor: "no_of_members", Header: "NO. OF MEMBERS" },
-      { accessor: "location", Header: "LOCATION" },
-      { accessor: "action", Header: "EVENT DETAILS" },
-    ],
-    []
-  )
+  const [selected, setSelected] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState({});
+  const [CompletedEventData, setCompletedEventData] = useState({});
 
   const [lineChartData, setLineChartData] = useState([
     [
@@ -99,16 +54,6 @@ const data = useMemo(
     
   ]);
 
-  useEffect(() => {
-    checkValidate();
-  }, []);
-
-  const checkValidate = async () => {
-    const y = localStorage.getItem("USER_KEY");
-    if (!y) {
-      window.location.href = "/";
-    }
-  };
   return (
     <>
       <div className="container-fluid calculated-bodywidth" style={{}} id="bla">
@@ -145,8 +90,47 @@ const data = useMemo(
             <div className="card h-100" id="contentcard">
               <div className="card-body ">
               <h5>Completed Events</h5><br></br>
+              <MaterialTable
+                    components={{
+                      Container: (props) => <Paper {...props} elevation={0} />,
+                    }}
+                    options={{ actionsColumnIndex: -1 }}
+                    title="My Completed Events"
+                    columns={[
+                      { field: "eventId", title: "EVENT ID" },
+                      { field: "category", title: "CATEGORY" },
+                      { field: "name", title: "COORDINATOR" },
+                      { field: "startDate", title: "STARTS ON" },
+                      { field: "endDate", title: "ENDS ON" },
+                      { field: "noOfVolunteers", title: "NO OF MEMBERS" },
+                      { field: "place", title: "LOCATION" },
+                    ]}
+                    // data={CompletedEventData}
+                    actions={[
+                      {
+                        icon: () => {
+                          return (
+                            <button
+                              type="button"
+                              className="btn mt-0"
+                              style={{
+                                backgroundColor: "#96BE25",
+                                border: "none",
+                              }}
+                            >
+                              Details
+                            </button>
+                          );
+                        },
+                        onClick: (event, rowData) => {
+                          // setSelectedJoinRequestsData(rowData);
+                          // setSelected(true);
+                        },
+                         tooltip: "Event Details",
+                      },
+                    ]}
+                    />
               
-              <NewTable columns={UpcomingEventsHeadings} data={upcomingEventsData} />
               </div>
             </div>
           </div>
