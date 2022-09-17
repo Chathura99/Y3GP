04a73daa6a75426fecb64c8952f9c72ed980e294
Shan-Dilranga <2019cs187@stu.ucsc.lk>
2022-09-18 01:02:36 +0530

    updated

--- vws-back-end/src/main/java/com/ucsc/vwsbackend/entities/Reply.java
+++ vws-back-end/src/main/java/com/ucsc/vwsbackend/entities/Reply.java
@@ -1,2 +1,109 @@
-package com.ucsc.vwsbackend.entities;public class Reply {
+package com.ucsc.vwsbackend.entities;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Table(name = "forum")
+@Entity
+public class Reply {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "forum_id")
+    private long forumId;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "start_date")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @CreationTimestamp
+    private Date startDate;
+
+    @Column(name = "end_date")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @CreationTimestamp
+    private Date endDate;
+
+    public long getForumId() {
+        return forumId;
+    }
+
+    public void setForumId(long forumId) {
+        this.forumId = forumId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public DiscussionTopic getDiscussionTopic() {
+        return discussionTopic;
+    }
+
+    public void setDiscussionTopic(DiscussionTopic discussionTopic) {
+        this.discussionTopic = discussionTopic;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "topic_id")
+    DiscussionTopic discussionTopic;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    User user;
 }
