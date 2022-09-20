a14b008d4c54956350e9b25a19af834c1fce26f9
Shan-Dilranga <2019cs187@stu.ucsc.lk>
2022-09-21 01:18:58 +0530

    changed

--- vws-back-end/src/main/java/com/ucsc/vwsbackend/controllers/ForumController.java
+++ vws-back-end/src/main/java/com/ucsc/vwsbackend/controllers/ForumController.java
@@ -1,2 +1,27 @@
-package com.ucsc.vwsbackend.controllers;public class ForumController {
+package com.ucsc.vwsbackend.controllers;
+
+
+import com.ucsc.vwsbackend.dto.ForumInfo;
+import com.ucsc.vwsbackend.dto.ProjectDetail;
+import com.ucsc.vwsbackend.services.ForumService;
+import com.ucsc.vwsbackend.services.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+@CrossOrigin(origins = "http://localhost:3000")
+public class ForumController {
+    @Autowired
+    ForumService forumService;
+
+    @GetMapping("/getForumInfo")
+    public List<ForumInfo> getForumInfo() {
+        return forumService.getForumInfo();
+    }
 }
--- vws-back-end/src/main/java/com/ucsc/vwsbackend/services/ForumService.java
+++ vws-back-end/src/main/java/com/ucsc/vwsbackend/services/ForumService.java
@@ -1,2 +1,23 @@
-package com.ucsc.vwsbackend.services;public class ForumService {
+package com.ucsc.vwsbackend.services;
+
+import com.ucsc.vwsbackend.dto.ForumInfo;
+import com.ucsc.vwsbackend.dto.ProjectDetail;
+import com.ucsc.vwsbackend.repository.announcementDao.AnnouncementJdbcRepository;
+import com.ucsc.vwsbackend.repository.announcementDao.AnnouncementRepository;
+import com.ucsc.vwsbackend.repository.forumDao.ForumJdbcRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ForumService {
+
+
+    @Autowired
+    ForumJdbcRepository forumJdbcRepository ;
+
+    public List<ForumInfo> getForumInfo() {
+        return forumJdbcRepository.getForumInfo();
+    }
 }
