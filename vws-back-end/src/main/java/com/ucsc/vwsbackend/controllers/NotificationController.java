3f74b7e4f527b6eadfa34ae19cc93bd8e2708733
Chathura <chathura.madura11@gmail.com>
2022-10-24 01:31:24 +0530

    create notification controller

--- vws-back-end/src/main/java/com/ucsc/vwsbackend/controllers/NotificationController.java
+++ vws-back-end/src/main/java/com/ucsc/vwsbackend/controllers/NotificationController.java
@@ -1,2 +1,21 @@
-package com.ucsc.vwsbackend.controllers;public class NotificationController {
+package com.ucsc.vwsbackend.controllers;
+
+import com.ucsc.vwsbackend.entities.Notification;
+import com.ucsc.vwsbackend.services.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+@CrossOrigin(origins = "http://localhost:3000")
+public class NotificationController {
+    @Autowired
+    NotificationService notificationService;
+
+    @GetMapping("/getUserNotification/{id}")
+    public List<Notification> getUserNotification(@PathVariable(value = "id") long id){
+        return notificationService.getUserNotification(id);
+    }
 }
