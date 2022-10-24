216f2adbc69098f6215affda63436296e28d663d
Chathura <chathura.madura11@gmail.com>
2022-10-24 20:01:47 +0530

    scholarship consideration queries

--- vws-back-end/src/main/java/com/ucsc/vwsbackend/repository/scholarshipDao/ScholarshipJdbcRepository.java
+++ vws-back-end/src/main/java/com/ucsc/vwsbackend/repository/scholarshipDao/ScholarshipJdbcRepository.java
@@ -1,2 +1,41 @@
-package com.ucsc.vwsbackend.repository.scholarshipDao;public class ScholarshipJdbcRepository {
+package com.ucsc.vwsbackend.repository.scholarshipDao;
+
+import com.ucsc.vwsbackend.entities.Scholarship;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class ScholarshipJdbcRepository {
+
+    @Autowired
+    protected NamedParameterJdbcTemplate jdbc;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    public List<Scholarship> getScholarship() {
+        String query ="SELECT * from scholarship";
+
+        List<Scholarship> scholarships = jdbc.query(query, new BeanPropertyRowMapper<Scholarship>(Scholarship.class));
+        return scholarships;
+    }
+
+    public int updateStatus(long id, Scholarship scholarship) {
+        MapSqlParameterSource namedParameters =
+                new MapSqlParameterSource();
+        String update = "UPDATE scholarship " +
+                "SET status = 1,description=:description,meeting_link=:link WHERE id = :id;";
+
+        namedParameters.addValue("id", id);
+        namedParameters.addValue("description", scholarship.getDescription());
+        namedParameters.addValue("link", scholarship.getMeetingLink());
+
+        int rowsAffected = jdbc.update(update, namedParameters);
+        return rowsAffected;
+    }
 }
--- vws-back-end/src/main/java/com/ucsc/vwsbackend/services/ScholarshipService.java
+++ vws-back-end/src/main/java/com/ucsc/vwsbackend/services/ScholarshipService.java
@@ -1,2 +1,37 @@
-package com.ucsc.vwsbackend.services;public class ScholarshipService {
+package com.ucsc.vwsbackend.services;
+
+import com.ucsc.vwsbackend.entities.Scholarship;
+import com.ucsc.vwsbackend.repository.scholarshipDao.ScholarshipJdbcRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ScholarshipService {
+    @Autowired
+    private JavaMailSender mailSender;
+    @Autowired
+    ScholarshipJdbcRepository scholarshipJdbcRepository;
+    public List<Scholarship> getScholarship() {
+        return  scholarshipJdbcRepository.getScholarship();
+    }
+
+    public long sendMeeting(Scholarship scholarship) {
+        SimpleMailMessage message=new SimpleMailMessage();
+        message.setFrom("vws.org2022@gmail.com");
+        message.setTo(scholarship.getEmail());
+
+        String content="Your application has considered! please join with the following meeting link\nLink : ";
+
+        message.setText("Welcome : "+scholarship.getFullName()+"\n"+ content + " " +scholarship.getMeetingLink()
+        +"\nNote: "+scholarship.getDescription());
+        message.setSubject("Welcome to the VWS!");
+        mailSender.send(message);
+
+        scholarshipJdbcRepository.updateStatus(scholarship.getId(),scholarship);
+        return  1;
+    }
 }
