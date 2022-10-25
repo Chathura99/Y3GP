089c1e6e876d28e85f8c2030a22f829e5da81782
Malik <2019cs193@stu.ucsc.lk>
2022-10-25 20:21:52 +0530

    new changes

--- vws-back-end/src/main/java/com/ucsc/vwsbackend/repository/ChartRepository.java
+++ vws-back-end/src/main/java/com/ucsc/vwsbackend/repository/ChartRepository.java
@@ -1,9 +1,6 @@
 package com.ucsc.vwsbackend.repository;
 
-import com.ucsc.vwsbackend.dto.AdminHomeSummary;
-import com.ucsc.vwsbackend.dto.AdminProjectSummary;
-import com.ucsc.vwsbackend.dto.UserSummary;
-import com.ucsc.vwsbackend.dto.VolunteerHomeSummary;
+import com.ucsc.vwsbackend.dto.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -85,5 +82,11 @@ public class ChartRepository {
         userSummary.setPcCount(jdbcTemplate.queryForObject(sql2, Integer.class));
 
         return  userSummary;
+    }
+    public List<VolunteerProjectSummary> getVolunteerProjectSummary(){
+        String sql ="SELECT count(*) as count,p.name FROM event as e INNER JOIN project as p " +
+                "ON p.project_id=e.project_id GROUP BY e.project_id";
+        List<VolunteerProjectSummary> summary = jdbc.query(sql, new BeanPropertyRowMapper<VolunteerProjectSummary>(VolunteerProjectSummary.class));
+        return summary;
     }
 }
