daf91e85c0e776386b4d685a14a0ecdba2370d82
Chathura <chathura.madura11@gmail.com>
2022-10-26 00:12:59 +0530

    changes in navbar guest

--- vws-front-end/src/pages/guestUser/LandingPageNavbar/navbar.js
+++ vws-front-end/src/pages/guestUser/LandingPageNavbar/navbar.js
@@ -1,55 +1,99 @@
-import React from 'react';
+import React from "react";
 import "./navbar.css";
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import { Link } from "react-scroll";
 
 export default function Navbar() {
-    return (
-        <>
+  return (
+    <>
+      <nav class="navbar navbar-expand-lg navbar-light" id="Navbar">
+        <a class="navbar-brand" href="#">
+          Sasnaka Logo
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon" id="nav-icon"></span>
+        </button>
 
-            <nav class="navbar navbar-expand-lg navbar-light" id="Navbar">
-                <a class="navbar-brand" href="#">Sasnaka Logo</a>
-                <button
-                    class="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation">
+        <div
+          class="collapse navbar-collapse"
+          id="navbarSupportedContent guestnavbar"
+        >
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <Link to="home-cont" spy={true} smooth={true}>
+                Home <span class="sr-only">(current)</span>
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link to="project-cont" spy={true} smooth={true}>
+                Our Projects
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link to="about-cont" spy={true} smooth={true}>
+                Support Us
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link to="about-cont" spy={true} smooth={true}>
+                About Us
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link to="contact-cont" spy={true} smooth={true}>
+                Contact Us
+              </Link>
+            </li>
+          </ul>
 
-                    <span class="navbar-toggler-icon"  id="nav-icon"></span>
-
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent guestnavbar">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Our Projects</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Support Us</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">About Us</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Contact Us</a>
-                        </li>
-                    </ul>
-
-                    <Link to="/login" smooth={true} spy={true}>
-                        <button class="btn my-2 my-sm-0" id="header-btn" Link to="/login" type="submit">Login</button>
-                    </Link>
-                    <Link to="/signup" smooth={true} spy={true}>
-                        <button class="btn my-2 my-sm-0" id="header-btn-2" type="submit">Sign Up</button>
-                    </Link>
-
-                </div>
-            </nav>
-
-        </>
-    )
+          {/* <Link to="/login" smooth={true} spy={true}>
+            <button
+              class="btn my-2 my-sm-0"
+              id="header-btn"
+              Link
+              to="/login"
+              type="submit"
+            >
+              Login
+            </button>
+          </Link> */}
+          <a href="/login">
+            <button
+              class="btn my-2 my-sm-0"
+              id="header-btn"
+              Link
+              to="/login"
+              type="submit"
+            >
+              Login
+            </button>
+          </a>
+          {/* <Link to="/signup" smooth={true} spy={true}>
+            <button class="btn my-2 my-sm-0" id="header-btn-2" type="submit">
+              Sign Up
+            </button>
+          </Link> */}
+          <a href="/signup">
+            <button
+              class="btn my-2 my-sm-0"
+              id="header-btn"
+              Link
+              to="/login"
+              type="submit"
+            >
+              Sign Up
+            </button>
+          </a>
+        </div>
+      </nav>
+    </>
+  );
 }
