e41156643f8c0a88d42fefac7d116bb8572270ea
Ravindu <ravindumedagama@gmail.com>
2022-10-26 20:50:34 +0530

    Changed the comment position

--- vws-front-end/src/pages/projectCoordinator/Volunteers/volunteer.js
+++ vws-front-end/src/pages/projectCoordinator/Volunteers/volunteer.js
@@ -41,6 +41,79 @@ export default function PcVolunteers() {
 
 
 
+return (
+    <>
+        <div className="container-fluid calculated-bodywidth" style={{}} id="bla">
+            
+
+            <div className="row gutters mt-3">
+                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                    <div className="card h-100" id="contentcard">
+                        <div className="card-body ">
+                        <div className="row gutters "></div>
+                            {/* <h5>Previous Events</h5> */}
+                            <div
+                              className="row gutters "
+                              style={{ justifyContent: "center" }}
+                            >
+                              <MaterialTable
+                                components={{
+                                  Container: (props) => <Paper {...props} elevation={0} />,
+                                }}
+                                options={{ actionsColumnIndex: -1 }}
+                                title="Previous Events"
+                                columns={[
+                                  { field: "eventId", title: "EVENT ID" },
+                                  { field: "category", title: "CATEGORY" },
+                                  { field: "name", title: "COORDINATOR" },
+                                  { field: "phoneNumber", title: "PHONE" },
+                                  { field: "startDate", title: "STARTED ON" },
+                                  { field: "endDate", title: "ENDS ON" },
+                                  { field: "noOfVolunteers", title: "NO OF MEMBERS" },
+                                  { field: "place", title: "LOCATION" },
+                                  { field: "status", title: "STATUS" },
+                                ]}
+                                data={previousEventData}
+                                actions={[
+                                  {
+                                    icon: () => {
+                                      return (
+                                        <button
+                                          type="button"
+                                          className="btn mt-0"
+                                          style={{
+                                            backgroundColor: "#96BE25",
+                                            border: "none",
+                                          }}
+                                        >
+                                          Progress
+                                        </button>
+                                      );
+                                    },
+                                    onClick: (event, rowData) => {
+                                      setEventProgressData(rowData)
+                                      // To Do - set progress
+                                      setSelected(true);
+                                    },
+                                    // tooltip: "Register User",
+                                  },
+                                ]}
+                              />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+);
+
+
+
+
+
+
+
 
 
 //
@@ -115,79 +188,6 @@ export default function PcVolunteers() {
 //        window.location.href = "/";
 //    }
 //};
-
-
-return (
-    <>
-        <div className="container-fluid calculated-bodywidth" style={{}} id="bla">
-            
-
-            <div className="row gutters mt-3">
-                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                    <div className="card h-100" id="contentcard">
-                        <div className="card-body ">
-                        <div className="row gutters "></div>
-                            {/* <h5>Previous Events</h5> */}
-                            <div
-                              className="row gutters "
-                              style={{ justifyContent: "center" }}
-                            >
-                              <MaterialTable
-                                components={{
-                                  Container: (props) => <Paper {...props} elevation={0} />,
-                                }}
-                                options={{ actionsColumnIndex: -1 }}
-                                title="Previous Events"
-                                columns={[
-                                  { field: "eventId", title: "EVENT ID" },
-                                  { field: "category", title: "CATEGORY" },
-                                  { field: "name", title: "COORDINATOR" },
-                                  { field: "phoneNumber", title: "PHONE" },
-                                  { field: "startDate", title: "STARTED ON" },
-                                  { field: "endDate", title: "ENDS ON" },
-                                  { field: "noOfVolunteers", title: "NO OF MEMBERS" },
-                                  { field: "place", title: "LOCATION" },
-                                  { field: "status", title: "STATUS" },
-                                ]}
-                                data={previousEventData}
-                                actions={[
-                                  {
-                                    icon: () => {
-                                      return (
-                                        <button
-                                          type="button"
-                                          className="btn mt-0"
-                                          style={{
-                                            backgroundColor: "#96BE25",
-                                            border: "none",
-                                          }}
-                                        >
-                                          Progress
-                                        </button>
-                                      );
-                                    },
-                                    onClick: (event, rowData) => {
-                                      setEventProgressData(rowData)
-                                      // To Do - set progress
-                                      setSelected(true);
-                                    },
-                                    // tooltip: "Register User",
-                                  },
-                                ]}
-                              />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </>
-);
-
-
-
-
-
 
 
 
